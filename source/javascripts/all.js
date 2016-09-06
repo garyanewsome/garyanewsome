8dc5ca172a4687ab202042ccaccc03d155c0bf33
Gary A. Newsome <garyanewsome@gmail.com>
2016-09-05 23:15:06 -0400

    Middleman Rebuild

---
+++ source/javascripts/all.js
@@ -0,0 +1,18 @@
+//= require jquery
+//= require materialize-sprockets
+
+$(".button-collapse").sideNav();
+
+// jQuery Scroll Animations
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 500);
+    }
+});
+
+
+
